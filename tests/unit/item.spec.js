f6e09889ccf1773b644c55c37adcc9055bf4874f
Matt Bernhardt <mjbernha@mit.edu>
2020-10-19 15:20:27 -0400

    More thorough tests for fields within search result object

--- tests/unit/item.spec.js
+++ tests/unit/item.spec.js
@@ -4,11 +4,24 @@ import Item from "@/components/Item.vue";
 describe("Item.vue", () => {
   it("renders props.result.title when passed", () => {
     const result = {
-    	title: 'The great American novel'
+      title: 'The great American novel',
+      content_type: 'Book',
+      contributors: [
+        {
+          "kind": "author",
+          "value": "John William De Forest"
+        }
+      ],
+      publication_date: '1868',
+      source_link: 'http://library.mit.edu/item/000544411'
     };
     const wrapper = shallowMount(Item, {
       props: { result }
     });
     expect(wrapper.text()).toMatch(result.title);
+    expect(wrapper.text()).toMatch(result.content_type);
+    expect(wrapper.text()).toMatch(result.contributors[0].value);
+    expect(wrapper.text()).toMatch(result.publication_date);
+    expect(wrapper.text()).toMatch(result.source_link);
   });
 });
