4376ae96c5164c111151a3de34a8aa627c349d0d
Matt Bernhardt <mjbernha@mit.edu>
2020-10-20 13:31:13 -0400

    Installs and configures Sentry for exception monitoring
    This also includes a commented-out call to a non-existent function in main.js
    Making this call active will produce an error that gets sent to Sentry if
    that integration is still working.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
+// Sentry
+import Vue from "vue";
+import * as Sentry from "@sentry/browser";
+import { Vue as VueIntegration } from "@sentry/integrations";
+import { Integrations } from "@sentry/tracing";
+// Vue
 import { createApp } from "vue";
 import App from "./App.vue";
 
+if (process.env.VUE_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    integrations: [
+      new VueIntegration({
+        Vue,
+        tracing: true
+      }),
+      new Integrations.BrowserTracing()
+    ],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0
+  });
+}
+
 createApp(App).mount("#app");
+
+// Uncomment this line to guarantee that a message is sent to Sentry.
+// myUndefinedFunction();
