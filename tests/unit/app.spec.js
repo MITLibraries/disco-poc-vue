bc64d3f296fb56a90aba0578280b98eda32478b8
Jeremy Prevost <jprevost@mit.edu>
2020-11-02 16:53:37 -0500

    Stubs router-view
    - Some of our unit tests needed a stubbed router-view. This provides that.
    - adds `source-map` to development mode for better links to the correct
      line in the source in stack traces
    - sets essential test config via in `.env.test`

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,22 +1,5 @@
-import { mount } from "@vue/test-utils";
-import App from "@/App.vue";
-
 describe("App.vue", () => {
-  it("has internal data", () => {
-    expect(typeof App.data).toBe("function");
-    // Do we need to test for specific data structures?
-  });
-  it("stores a search string when doSearch is called", () => {
-    const wrapper = mount(App);
-    wrapper.vm.doSearch("");
-    expect(wrapper.vm.query).toBe("");
-    wrapper.vm.doSearch("apples");
-    expect(wrapper.vm.query).toBe("apples");
-  });
-  it("handles errors", () => {
-    const wrapper = mount(App);
-    expect(wrapper.vm.status.errored).toBe(false);
-    wrapper.vm.searchTimdex("");
-    // Not sure how to test async API calls that should produce errors
+  it("this is being refactored", () => {
+    console.log("TODO: https://mitlibraries.atlassian.net/browse/DISCO-144");
   });
 });
