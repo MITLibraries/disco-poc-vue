72b2ff2272cf210a0fa23d1989a436fdd52ec14f
Jeremy Prevost <jprevost@mit.edu>
2020-11-04 16:53:30 -0500

    Adds unit tests for Record.vue
    Handles normal responses as well as record not found and generic
    timdex errors.
    
    Also tries to recover from invalid timdex record (this may not be a
    great idea?) by displaying "Unknown title" if one isn't provided and
    displaying whatever else was provided.
    
    We're using axios mocks via Jest's built in mocking capabilities for
    this. We modified some portion of the TIMDEX call to allow for easier
    testing.
    
    https: //mitlibraries.atlassian.net/browse/DISCO-152
    Co-Authored-By: Matthew Bernhardt <mjbernha@mit.edu>
    Co-Authored-By: Adam Jazairi <16103405+jazairi@users.noreply.github.com>

--- tests/unit/record.spec.js
+++ tests/unit/record.spec.js
@@ -1,5 +1,241 @@
-describe("Record.vue", () => {
-  it("renders title from result data", () => {
-    console.log("TODO: https://mitlibraries.atlassian.net/browse/DISCO-152");
+import { mount } from "@vue/test-utils";
+import axios from "axios";
+import Record from "@/views/Record.vue";
+
+var mockResponse = {};
+
+jest.mock("axios");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("valid id", () => {
+  it("loads data", async () => {
+    mockResponse = {
+      status: 200,
+      data: {
+        id: "002574584",
+        title: "Cheese",
+        source: "MIT Aleph"
+      }
+    };
+
+    axios.get.mockImplementation(() => Promise.resolve(mockResponse));
+
+    const mockRoute = {
+      params: {
+        recordId: "002574584"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(Record, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://timdex.example.com/api/v1/record/002574584"
+    );
+
+    expect(wrapper.vm.$data.status.loading).toBe(true);
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$data.status.loading).toBe(false);
+      expect(wrapper.text()).toMatch("Cheese");
+      expect(wrapper.vm.$data.result.id).toBe("002574584");
+      expect(wrapper.vm.$data.result.title).toBe("Cheese");
+    });
+  });
+});
+
+describe("invalid id", () => {
+  it("displays 404", async () => {
+    mockResponse = {
+      response: {
+        status: 404,
+        statusText: "Not Found",
+        data: {
+          error: "record not found"
+        }
+      }
+    };
+
+    axios.get.mockImplementation(() => Promise.reject(mockResponse));
+
+    const mockRoute = {
+      params: {
+        recordId: "asdf"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(Record, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://timdex.example.com/api/v1/record/asdf"
+    );
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$data.status.loading).toBe(false);
+      expect(wrapper.text()).toMatch("The requested record was not found.");
+    });
+  });
+});
+
+describe("non 404 error", () => {
+  it("displays unknown error", async () => {
+    mockResponse = {
+      response: {
+        status: 500,
+        statusText: "Internal Server Error"
+      }
+    };
+
+    axios.get.mockImplementation(() => Promise.reject(mockResponse));
+
+    const mockRoute = {
+      params: {
+        recordId: "asdf"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(Record, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://timdex.example.com/api/v1/record/asdf"
+    );
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$data.status.loading).toBe(false);
+      expect(wrapper.text()).toMatch("An unknown error occured.");
+    });
+  });
+});
+
+describe("sparsest valid timdex record", () => {
+  it("doesn't error on any non-provided fields", async () => {
+    mockResponse = {
+      status: 200,
+      data: {
+        id: "002574584",
+        title: "Cheese"
+      }
+    };
+
+    axios.get.mockImplementation(() => Promise.resolve(mockResponse));
+
+    const mockRoute = {
+      params: {
+        recordId: "002574584"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(Record, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://timdex.example.com/api/v1/record/002574584"
+    );
+
+    expect(wrapper.vm.$data.status.loading).toBe(true);
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$data.status.loading).toBe(false);
+      expect(wrapper.text()).toMatch("Cheese");
+      expect(wrapper.vm.$data.result.id).toBe("002574584");
+      expect(wrapper.vm.$data.result.title).toBe("Cheese");
+    });
+  });
+});
+
+describe("invalid timdex record", () => {
+  it("displays what it can", async () => {
+    mockResponse = {
+      status: 200,
+      data: {
+        languages: ["English"],
+        physical_description: "Dusty old box of cheese"
+      }
+    };
+
+    axios.get.mockImplementation(() => Promise.resolve(mockResponse));
+
+    const mockRoute = {
+      params: {
+        recordId: "002574584"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(Record, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      "https://timdex.example.com/api/v1/record/002574584"
+    );
+
+    expect(wrapper.vm.$data.status.loading).toBe(true);
+
+    await wrapper.vm.$nextTick(() => {
+      expect(wrapper.vm.$data.status.loading).toBe(false);
+      expect(wrapper.text()).toMatch("Unknown title");
+      expect(wrapper.text()).toMatch("English");
+      expect(wrapper.text()).toMatch("Dusty old box of cheese");
+      expect(wrapper.vm.$data.result.title).toBeNull;
+    });
   });
 });
