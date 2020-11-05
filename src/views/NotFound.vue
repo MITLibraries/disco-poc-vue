e811b8a3d23e68d3d7ca526f78256d6b6adc1b48
Adam Jazairi <jazairi@mit.edu>
2020-11-05 17:05:42 -0500

    Add a route and component for 404 errors
    This creates a catch-all route, such that any request resulting
    in a 404 error will render a custom view (NotFound.vue) that lets
    the user know that page doesn't exist and directs them back to
    the root route.

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrap-outer-content layout-band">
+    <div class="wrap-content">
+      <main id="content-main" class="content-main" role="main">
+        <h2 class="title title-page">
+          Oops! The requested page was not found.
+        </h2>
+
+        <p>
+          <a href="/">Starting over</a> may help. If it does not, please
+          <a href="https://libraries.mit.edu/ask/">let us know</a>!
+        </p>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound"
+};
+</script>
---
+++ tests/unit/notfound.spec.js
@@ -0,0 +1,30 @@
+import { mount } from "@vue/test-utils";
+import NotFound from "@/views/NotFound";
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("NotFound.vue", () => {
+  it("displays custom 404 page", async () => {
+    const mockRoute = {
+      params: {
+        name: "flerb"
+      }
+    };
+    const mockRouter = {
+      push: jest.fn()
+    };
+
+    const wrapper = mount(NotFound, {
+      global: {
+        mocks: {
+          $route: mockRoute,
+          $router: mockRouter
+        }
+      }
+    });
+
+    expect(wrapper.text()).toMatch("Oops! The requested page was not found.");
+  });
+});
